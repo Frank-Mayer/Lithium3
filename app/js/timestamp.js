0fb5692f0a5d3f9c009b1347ade202816a0f58f0
RedStylzZ <tizian.appler@gmail.com>
2020-07-05 18:48:32 +0200

    BugFix Timestamp

--- app/js/timestamp.js
+++ app/js/timestamp.js
@@ -1,38 +1,37 @@
 "use strict";
 
-class Timestamp {
-  date = new Object();
+class Timestamp extends Date {
 
   dateToTimestamp() {
     return Number(
-      this.date.getFullYear().toString().padStart(4, "0") +
-        (this.date.getMonth() + 1).toString().padStart(2, "0") +
-        this.date.getDate().toString().padStart(2, "0") +
-        this.date.getHours().toString().padStart(2, "0") +
-        this.date.getMinutes().toString().padStart(2, "0") +
-        this.date.getSeconds().toString().padStart(2, "0") +
-        this.date.getMilliseconds().toString().padStart(3, "0")
+      this.getFullYear().toString().padStart(4, "0") +
+        (this.getMonth() + 1).toString().padStart(2, "0") +
+        this.getDate().toString().padStart(2, "0") +
+        this.getHours().toString().padStart(2, "0") +
+        this.getMinutes().toString().padStart(2, "0") +
+        this.getSeconds().toString().padStart(2, "0") +
+        this.getMilliseconds().toString().padStart(3, "0")
     );
   }
 
   timestampToDate(foo) {
     foo = String(foo);
-    this.date.setFullYear = Number(foo.substr(0, 4));
-    this.date.setMonth = Number(foo.substr(4, 2)) - 1;
-    this.date.setDate = Number(foo.substr(6, 2));
-    this.date.setHours = Number(foo.substr(8, 2));
-    this.date.setMinutes = Number(foo.substr(10, 2));
-    this.date.setSeconds = Number(foo.substr(12, 2));
-    this.date.setMilliseconds = Number(foo.substr(14, 3));
+    this.setFullYear(Number(foo.substr(0, 4)));
+    this.setMonth(Number(foo.substr(4, 2)) - 1);
+    this.setDate(Number(foo.substr(6, 2)));
+    this.setHours(Number(foo.substr(8, 2)));
+    this.setMinutes(Number(foo.substr(10, 2)));
+    this.setSeconds(Number(foo.substr(12, 2)));
+    this.setMilliseconds(Number(foo.substr(14, 3)));
   }
 
   toString() {
     return String(
-        String(this.date.getHours()).padStart(2, "0") +
+        String(this.getHours()).padStart(2, "0") +
         ":" +
-        String(this.date.getMinutes()).padStart(2, "0") +
+        String(this.getMinutes()).padStart(2, "0") +
         ":" +
-        String(this.date.getSeconds()).padStart(2, "0")
+        String(this.getSeconds()).padStart(2, "0")
     );
   }
 }
