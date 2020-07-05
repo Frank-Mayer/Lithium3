fd4d38ecacdd9dc4c199d8ab42a1df2e5b942989
Frank Mayer <frank.mayer19072001@outlook.de>
2020-07-05 11:59:37 +0200

    Docs Menu bugfix, mobile not working

--- app/docs/sript.js
+++ app/docs/sript.js
@@ -14,19 +14,41 @@ function main() {
     for (let i = 0; i < content.length; i++) {
       content[i].classList = (index === i ? "content" : "content invis");
     }
+    setHash(index);
   }
-  document.getElementById("b1").addEventListener("click", () => {
-    tab(0)
-  });
-  document.getElementById("b2").addEventListener("click", () => {
-    tab(1)
-  });
-  document.getElementById("b3").addEventListener("click", () => {
-    tab(2)
-  });
-  tab(1);
+  let b1 = document.getElementsByClassName("b1");
+  let b2 = document.getElementsByClassName("b2");
+  let b3 = document.getElementsByClassName("b3");
+  for (let i = 0; i < b1.length; i++) {
+    b1[i].addEventListener("click", () => {
+      tab(0);
+    });
+  }
+  for (let i = 0; i < b2.length; i++) {
+    b2[i].addEventListener("click", () => {
+      tab(1)
+    });
+  }
+  for (let i = 0; i < b3.length; i++) {
+    b3[i].addEventListener("click", () => {
+      tab(2)
+    });
+  }
+  let hash = Number(location.hash.substr(1));
+  if (hash >= 0 && hash <= 3) {
+    tab(hash)
+  }
 }
 
 function scr(id) {
   document.getElementById(id).scrollIntoView(true);
+}
+
+function setHash(str) {
+  if (history.pushState) {
+    history.pushState(null, null, '#' + str);
+  }
+  else {
+    location.hash = '#' + str;
+  }
 }
