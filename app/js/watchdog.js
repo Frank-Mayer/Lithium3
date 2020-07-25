1a42ad8807eb07d3e09aed34e3721e5e15062a16
Frank Mayer <frank.mayer19072001@outlook.de>
2020-07-25 20:07:09 +0200

    Security check added

--- app/js/watchdog.js
+++ app/js/watchdog.js
@@ -2,6 +2,23 @@
 
 class WatchDog {
   constructor() {
-    this.loginControl = document.getElementById("loginControl");
+    if (window.self !== window.top) {
+      window.location.href = "./405.html";
+      throw "fatal error";
+    }
+    else {
+      let embed = document.createElement("embed");
+      embed.src = `./app.html${window.location.hash}`;
+      embed.style.position = "absolute";
+      embed.style.top = "0";
+      embed.style.right = "0";
+      embed.style.bottom = "0";
+      embed.style.left = "0";
+      embed.style.width = "100vw";
+      embed.style.height = "100vh";
+      document.body.appendChild(embed);
+    }
   }
 }
+
+let wd = new WatchDog();
