9267411cb480ea1753a9828cbac023b94c757b5c
Frank Mayer <frank.mayer19072001@outlook.de>
2020-07-25 20:22:02 +0200

    Style Fix

--- app/js/watchdog.js
+++ app/js/watchdog.js
@@ -9,13 +9,6 @@ class WatchDog {
     else {
       let embed = document.createElement("embed");
       embed.src = `./app.html${window.location.hash}`;
-      embed.style.position = "absolute";
-      embed.style.top = "0";
-      embed.style.right = "0";
-      embed.style.bottom = "0";
-      embed.style.left = "0";
-      embed.style.width = "100vw";
-      embed.style.height = "100vh";
       document.body.appendChild(embed);
     }
   }
